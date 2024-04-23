e61d39c26334ef6950d43390c4052029432f0fc0
manoj-palagiri <manojpalagiri@gmail.com>
2024-04-24 00:27:02 +0530

    commit

---
+++ Backend/backend/todo/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('todo',views.TodoViewSet, basename='todo')
+
+urlpatterns = [
+    
+]
+
+urlpatterns += router.urls
+
